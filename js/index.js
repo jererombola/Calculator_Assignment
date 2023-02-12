2129891a06358d329f18a9a46c32bb98d6a99319
Topliceanu Eduard <topliceanu.eduard562@gmail.com>
2023-02-12 03:21:21 +0200

    Fixed the "Users should be able to string together several operations and get the right answer", added to work on keydowns and minor css. ONLY "Enter" button I did not finish!

--- js/index.js
+++ js/index.js
@@ -1,146 +1,129 @@
-let calculator = document.getElementById("calculator");
-let buttons= Array.from(document.querySelectorAll("button"))
-let screen = document.getElementById("answer")
-let screenValue = ""; 
-let numbers = ["1","2","3","4","5","6","7","8","9","0"]
-let operators = ["+", "-", "/", "*", "%",];
+let calculator = document.getElementById('calculator');
+let buttons = Array.from(document.querySelectorAll('button'));
+let screen = document.getElementById('answer');
+let screenValue = '';
+let numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+let operators = ['+', '-', '/', '*', '%'];
+let operand1 = 0;
+let operand2 = 0;
+let operator = '';
 
-const addition = (operand1, operand2) => operand1 + operand2;
+const addition = (operand1, operand2) => +operand1 + +operand2;
 const subtraction = (operand1, operand2) => operand1 - operand2;
 const multiplication = (operand1, operand2) => operand1 * operand2;
 const division = (operand1, operand2) => operand1 / operand2;
 const percentage = (operand1) => operand1 / 100;
-const minusMax = (operand1) => operand1 * (-1)
+const minusMax = (operand1) => operand1 * -1;
 
-
-function operate(operand1, operator, operand2) {
-    switch (operator) {
-        case '/':
-            return division(operand1, operand2);
-        case '*':
-            return multiplication(operand1, operand2);
-        case '+':
-            return addition(operand1, operand2);
-        case '-':
-            return subtraction(operand1, operand2);
-        case '%':
-            return percentage(operand1);
-        case '+/-':
-            return minusMax(operand1);
-        default:
-            break;
-    }
+function operate() {
+  switch (operator) {
+    case '/':
+      return division(operand1, operand2);
+    case '*':
+      return multiplication(operand1, operand2);
+    case '+':
+      return addition(operand1, operand2);
+    case '-':
+      return subtraction(operand1, operand2);
+    case '%':
+      return percentage(operand1);
+    case '+/-':
+      return minusMax(operand1);
+    default:
+      break;
+  }
 }
-operate();
 
-buttons.forEach(button=>{
-    button.addEventListener("click", function(event) {
-        event.preventDefault();
-        let buttonPress = event.target.innerText;
-        if (buttonPress == "A/C"){
-            screenValue = "";
-            screen.value = screenValue;
-        }
-        else if (buttonPress == "Back"){
-            screenValue = screenValue.slice(0,-1)
-            screen.value = screenValue
-        }
-        else if(buttonPress === "=" && screenValue.indexOf("/0") != -1){
-            screenValue = "ERR0R!!!"
-            screen.value = screenValue
-        }
-        else if(buttonPress === "." && screenValue.indexOf(".") != -1){
-            screen.value = screenValue;
-        } 
-        else if(buttonPress == "+/-"){
-            screen.value = operate(screenValue, "+/-", "")
-            screenValue = screen.value
+function prepareCalc(buttonPress, screenValue) {
+  if (operator === '') {
+    operator = buttonPress;
+  } else {
+    operand2 = screen.value;
+    let total = operate();
+    screen.value = total;
+    operand1 = total;
+    operand2 = 0;
+    operator = buttonPress;
+  }
 
-        }
-        else if(buttonPress == "%"){
-           screen.value = operate(screenValue, "%", "")
-           screenValue = screen.value
+  if (operand1 === 0) {
+    operand1 = screenValue;
+    screen.value = '';
+  }
+}
 
-        }
-        else if(buttonPress === "="){
-            let calculation = screenValue.split('');
-            let operation, answer, operand1, operand2;
-            for (let i = 0; i < calculation.length; i++) {
-                if (calculation[i] === "/" || calculation[i] === '*' || calculation[i] === "+" || calculation[i] === "-") {
-                    let operationPos = calculation.indexOf(calculation[i]);
-                    operation = calculation[i];
-                    operand1 = parseFloat(calculation.slice(0, operationPos).join(''));
-                    operand2 = parseFloat(calculation.slice(operationPos + 1).join(''));
-                    answer = operate(operand1, operation, operand2);
-                    screen.value = (answer);
-                    screenValue = screen.value
-                    console.log(screenValue);
+buttons.forEach((button) => {
+  button.addEventListener('click', function (event) {
+    event.preventDefault();
+    takeInput(event.target.innerText);
+  });
+});
 
-                  }
-                }
-            }
+function takeInput(input) {
+  switch (input) {
+    case 'A/C':
+      operand1 = 0;
+      operand2 = 0;
+      operator = '';
+      screen.value = '';
+      break;
+    case 'Back':
+      screen.value = screen.value.slice(0, -1);
+      break;
+    case 'Backspace':
+      screen.value = screen.value.slice(0, -1);
+      break;
+    case '=':
+      if (screen.value.indexOf('/0') != -1) {
+        screen.value = 'ERR0R!!!';
+      } else {
+        operand2 = screen.value;
+        let total = operate();
+        screen.value = total;
+        operand1 = total;
+        operand2 = 0;
+        operator = '';
+      }
+      break;
+    case 'Enter':
+      // Work in progress (need help) //
 
-        else{
-            screenValue += buttonPress;
-            screen.value = screenValue
-        }
-    })
-})
+      break;
+    case '.':
+      if (!screen.value.includes('.') && screen.value != '') {
+        screen.value += '.';
+      }
+      break;
+    case '+/-':
+      screen.value = operate();
+      break;
+    case '%':
+      screen.value = operate();
+      break;
+    case '/':
+      prepareCalc(input, screen.value);
+      break;
+    case '*':
+      prepareCalc(input, screen.value);
+      break;
+    case '-':
+      prepareCalc(input, screen.value);
+      break;
+    case '+':
+      prepareCalc(input, screen.value);
+      break;
+    default: {
+      if (screen.value == operand1 && screen.value != '') {
+        screen.value = input;
+      } else {
+        screen.value = screen.value + input;
+      }
+    }
+  }
+}
 
-buttons.forEach(keys=>{
-    keys.addEventListener("keydown", function(event) {
-        let keypress = event.key
-        numbers.forEach(number => {
-            if(keypress == number){
-                screenValue += (keypress);
-                screen.value = screenValue;
-            }
-        })
-           
-        operators.forEach(operator => {
-        if(keypress == operator){
-                screenValue += keypress;
-                screen.value = screenValue;
-                console.log(keypress);
-            }
-        })
-
-        if(keypress === "." && screenValue.indexOf(".") != -1){
-            screen.value = screenValue;
-        } 
-        else if(keypress === "Enter" && screenValue.indexOf("/0") != -1){
-            screenValue = "ERR0R!!!"
-            screen.value = screenValue
-        }
-        else if(keypress === "Enter"){
-            let calculation = screenValue.split('');
-            let operation, answer, operand1, operand2;
-            for (let i = 0; i < calculation.length; i++) {
-                if (calculation[i] === "/" || calculation[i] === '*' || calculation[i] === "+" || calculation[i] === "-") {
-                    let operationPos = calculation.indexOf(calculation[i]);
-                    operation = calculation[i];
-                    operand1 = parseFloat(calculation.slice(0, operationPos).join(''));
-                    operand2 = parseFloat(calculation.slice(operationPos + 1).join(''));
-                    answer = operate(operand1, operation, operand2);
-                    screen.value = (answer);
-                    screenValue = screen.value
-                    console.log(screenValue);
-                }
-           }
-        }
-        else if(keypress === "%"){
-            screen.value = operate(parseFloat(screenValue), "%", "")
-            screenValue = screen.value
-
-        }
-        else if (keypress == "Backspace"){
-            screenValue = screenValue.slice(0,-1)
-            screen.value = screenValue
-        }
-        else if (keypress == "Escape"){
-            screenValue = "";
-            screen.value = screenValue;
-        }
-       
-    })
-})
+buttons.forEach((keys) => {
+  keys.addEventListener('keydown', function (event) {
+    takeInput(event.key);
+  });
+});
